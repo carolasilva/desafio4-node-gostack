8946a9724b4684d1f33090886739d757b9197200
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-06-24 10:58:59 -0300

    create and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface GetAllTransactionsDTO {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,61 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): GetAllTransactionsDTO {
+    const response = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return response;
   }
+
+  private isIncome = (transaction: Transaction): boolean =>
+    transaction.type === 'income';
+
+  private isOutcome = (transaction: Transaction): boolean =>
+    transaction.type === 'outcome';
+
+  private sum = (a: number, b: number): number => a + b;
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    if (this.transactions.length > 0) {
+      const incomeTransactions = this.transactions.filter(this.isIncome);
+      const incomeTransactionsValues = incomeTransactions.map(
+        transaction => transaction.value,
+      );
+      income = incomeTransactionsValues.reduce(this.sum, 0);
+
+      const outcomeTransactions = this.transactions.filter(this.isOutcome);
+      const outcomeTransactionsValues = outcomeTransactions.map(
+        transaction => transaction.value,
+      );
+      outcome = outcomeTransactionsValues.reduce(this.sum, 0);
+    }
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public isOutcomeAmmountValid(ammount: number): boolean {
+    const { total } = this.getBalance();
+
+    return ammount <= total;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
